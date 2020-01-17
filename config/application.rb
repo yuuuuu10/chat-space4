481c3ab0455d2d26b46e8ae05b7acec05078afda
yuuuuu10 <yuto0105@ruri.waseda.jp>
2020-01-17 12:18:07 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ChatSpace4
+  class Application < Rails::Application
+    config.time_zone = "Tokyo"
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      config.i18n.default_locale = :ja
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "groups#index"
+  get 'messages/index'
+  resources :users, only: [:index,:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
+  end
+end
